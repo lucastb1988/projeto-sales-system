9d05086291798a296f5e8a613796af46707606c1
Lucas Banin <lucas.t.banin@gmail.com>
2018-08-13 15:46:22 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/br/com/psgv/sale/resources/CategoriaResource.java
+++ src/main/java/br/com/psgv/sale/resources/CategoriaResource.java
@@ -2,15 +2,16 @@ package br.com.psgv.sale.resources;
 
 import java.net.URI;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -69,6 +70,23 @@ public class CategoriaResource {
 		//map vai efetuar uma operação para cada elemento da lista utilizando java8
 		//para transformar em listDto no final da função inserir o list no collect(Collectors.toList())
 		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		
+		//gera response 200(ok)
+		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			//@RequestParam similar ao @QueryParam (não é obrigatorio informar caso não tenha validação)
+			@RequestParam(value = "page", defaultValue = "0") Integer page, 
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy, 
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+		
+		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+		
+		//Page já é java8 compliance então não é necessario passar o stream e collect(Collectors.toList()) como no serviço findAll acima
+		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));
 		
 		//gera response 200(ok)
 		return ResponseEntity.ok().body(listDto);

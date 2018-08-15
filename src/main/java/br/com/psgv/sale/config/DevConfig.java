afeca3638c906e6460aea9d1d580b00a43a6c5aa
Lucas Banin <lucas.t.banin@gmail.com>
2018-08-15 11:24:55 -0300

    Criando Profile de Desenvolvimento

---
+++ src/main/java/br/com/psgv/sale/config/DevConfig.java
@@ -0,0 +1,37 @@
+package br.com.psgv.sale.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.psgv.sale.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy; // captura a chave dentro de application.properties
+	
+	@Bean
+	public boolean instantiateDataBase() throws ParseException {
+		
+		//se a chave spring.jpa.hibernate.ddl-auto dentro de application.properties 
+		//for diferente de "create" não executa a chamada ao banco para criar todas as tabelas novamente 
+		if (!"create".equals(strategy)) { 
+			return false;
+		}
+		
+		dbService.instantiateTestDataBase();
+		
+		return true;
+	}
+
+}

3e8d633433d2d19c21183f5d233e2e089d7ff5f8
Lucas Banin <lucas.t.banin@gmail.com>
2018-08-18 14:20:07 -0300

    Tratando excecões adequadamente

--- src/main/java/br/com/psgv/sale/services/S3Service.java
+++ src/main/java/br/com/psgv/sale/services/S3Service.java
@@ -15,6 +15,8 @@ import org.springframework.web.multipart.MultipartFile;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 
+import br.com.psgv.sale.exceptions.FileException;
+
 @Service
 public class S3Service {
 	
@@ -36,7 +38,7 @@ public class S3Service {
 			return uploadFile(is, fileName, contentType);
 
 		} catch (IOException e) {
-			throw new RuntimeException("Erro de IO: " + e.getMessage());
+			throw new FileException("Erro de IO: " + e.getMessage());
 		}
 	}
 
@@ -53,7 +55,7 @@ public class S3Service {
 			return s3client.getUrl(bucketName, fileName).toURI();
 			
 		} catch (URISyntaxException e) {
-			throw new RuntimeException("Erro ao converter URL em URI");
+			throw new FileException("Erro ao converter URL em URI");
 		}
 	}
 }

30c48d8dff00096043e73414fb94e238df0bff51
Lucas Banin <lucas.t.banin@gmail.com>
2018-08-20 09:42:11 -0300

    Endpoint para buscar cliente por email

--- src/main/java/br/com/psgv/sale/services/DBService.java
+++ src/main/java/br/com/psgv/sale/services/DBService.java
@@ -69,20 +69,46 @@ public class DBService {
 		
 		Categoria cat1 = new Categoria(null, "Informática");
         Categoria cat2 = new Categoria(null, "Escritório");
+        Categoria cat3 = new Categoria(null, "Cama, mesa e banho");
+        Categoria cat4 = new Categoria(null, "Eletrônicos");
+        Categoria cat5 = new Categoria(null, "Jardinagem");
+        Categoria cat6 = new Categoria(null, "Itens para casa");
+        Categoria cat7 = new Categoria(null, "Linha odonto");
         
         Produto p1 = new Produto(null, "Computador", 2000.00);
         Produto p2 = new Produto(null, "Impressora", 800.00);
         Produto p3 = new Produto(null, "Mouse", 80.00);
+        Produto p4 = new Produto(null, "Mesa", 1000.00);
+        Produto p5 = new Produto(null, "Tapete Colorido", 300.00);
+        Produto p6 = new Produto(null, "Tapete", 500.00);
+        Produto p7 = new Produto(null, "TV", 3000.00);
+        Produto p8 = new Produto(null, "Chapéu", 80.00);
+        Produto p9 = new Produto(null, "Abajur", 150.00);
+        Produto p10 = new Produto(null, "Lustre", 2500.00);
+        Produto p11 = new Produto(null, "Shampoo", 12.00);
         
         cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
         cat2.getProdutos().addAll(Arrays.asList(p2));
+        cat3.getProdutos().addAll(Arrays.asList(p4, p5, p6));
+        cat4.getProdutos().addAll(Arrays.asList(p7));
+        cat5.getProdutos().addAll(Arrays.asList(p8));
+        cat6.getProdutos().addAll(Arrays.asList(p9, p10));
+        cat7.getProdutos().addAll(Arrays.asList(p11));
         
         p1.getCategorias().addAll(Arrays.asList(cat1));
         p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
         p3.getCategorias().addAll(Arrays.asList(cat1));
+        p4.getCategorias().addAll(Arrays.asList(cat3));
+        p5.getCategorias().addAll(Arrays.asList(cat3));
+        p6.getCategorias().addAll(Arrays.asList(cat3));
+        p7.getCategorias().addAll(Arrays.asList(cat4));
+        p8.getCategorias().addAll(Arrays.asList(cat5));
+        p9.getCategorias().addAll(Arrays.asList(cat6));
+        p10.getCategorias().addAll(Arrays.asList(cat6));
+        p11.getCategorias().addAll(Arrays.asList(cat7));
         
-        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-        produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
+        produtoRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11));
         
         Estado est1 = new Estado(null, "Minas Gerais");
         Estado est2 = new Estado(null, "São Paulo");
